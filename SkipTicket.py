f4e3b6451f3802847b12b3b6d1ca962fa0b36d7c
David Zeng <castinglok@gmail.com>
2021-08-28 03:11:09 -0400

    adding constant for sleep time

--- SkipTicket.py
+++ SkipTicket.py
@@ -3,6 +3,9 @@ import keyboard
 import win32api, win32con
 import time
 
+#Sleep Timer
+SleepTime = 0.8
+
 def click(x,y):
     win32api.SetCursorPos((x,y))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
@@ -25,20 +28,20 @@ def lookForReplay():
 
             if(Confirm_POS != None):
                 click(Confirm_POS[0], Confirm_POS[1])
-                time.sleep(0.8)
+                time.sleep(SleepTime)
             if(Replay_POS != None):
                 click(Replay_POS[0], Replay_POS[1])
                 counter += 1
-                time.sleep(0.8)
+                time.sleep(SleepTime)
             if(Next_POS != None and Replay_POS == None):
                 click(Next_POS[0], Next_POS[1])
-                time.sleep(0.8)
+                time.sleep(SleepTime)
             if(rankup_POS != None):
                 click(rankup_POS[0], rankup_POS[1])
-                time.sleep(0.8)
+                time.sleep(SleepTime)
             if(affinity_POS != None):
                 click(affinity_POS[0], affinity_POS[1])
-                time.sleep(0.8)
+                time.sleep(SleepTime)
 
         except:
             continue
